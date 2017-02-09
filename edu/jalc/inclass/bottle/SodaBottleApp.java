d7f8a87349d76f638d26b0ef8ec1664a69fc000a
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-08 21:23:47 -0600

    Uploading Old Files

---
+++ edu/jalc/inclass/bottle/SodaBottleApp.java
@@ -0,0 +1,8 @@
+package edu.jalc.inclass.bottle;
+class SodaBottleApp{
+  public static void main(String[] args){
+    SodaBottle sodaBottle = new SodaBottle();
+    System.out.println(sodaBottle.getCapacity());
+    // running a test use assert(condition)
+  }
+}
