7e110ec392bec23ebd605c83b6e6dd0d794c7a09
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-08 21:19:25 -0600

    Soda Bottle

---
+++ edu/jalc/inclass/bottle/SodaBottle.java
@@ -0,0 +1,9 @@
+package edu.jalc.inclass.bottle;
+class SodaBottle{
+  double height;
+  private final double capacity = 20.0;
+  boolean isEmpty;
+  double getCapacity(){
+    return capacity;
+  }
+}
