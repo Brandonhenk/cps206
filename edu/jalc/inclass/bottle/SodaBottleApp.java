d0ffdd006edc3281cf723505b2125fe2ef586221
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-08 21:44:11 -0600

    Updating SodaBottleApp

--- edu/jalc/inclass/bottle/SodaBottleApp.java
+++ edu/jalc/inclass/bottle/SodaBottleApp.java
@@ -1,8 +1,11 @@
 package edu.jalc.inclass.bottle;
-class SodaBottleApp{
-  public static void main(String[] args){
-    SodaBottle sodaBottle = new SodaBottle();
-    System.out.println(sodaBottle.getCapacity());
-    // running a test use assert(condition)
-  }
+
+import edu.jalc.inclass.lid.SodaBottleLid;
+
+public class SodaBottleApp {
+
+	public static void main(String... args) throws Exception {
+		SodaBottle sodaBottle = new SodaBottle(20, new SodaBottleLid(1, 2));
+		System.out.println("Capacity: " + sodaBottle.getCapacity());
+	}
 }
