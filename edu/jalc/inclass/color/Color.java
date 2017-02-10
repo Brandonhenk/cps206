8bf6b05c9a5006d50dcad2454b26883bf7fc2712
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-09 22:57:09 -0600

    Update for Color

--- edu/jalc/inclass/color/Color.java
+++ edu/jalc/inclass/color/Color.java
@@ -1,1 +1,28 @@
 package edu.jalc.inclass.color;
+
+public class Color{
+
+  private final byte red;
+  private final byte blue;
+  private final byte green;
+
+  private Color(){
+    this.red = this.blue = this.green = 0;
+  }
+
+  public Color(byte red, byte blue, byte green){
+    this.red = red;
+    this.blue = blue;
+    this.green = green;
+  }
+
+  public byte getRed(){
+    return red;
+  }
+  public byte getBlue(){
+    return blue;
+  }
+  public byte getGreen(){
+    return green;
+  }
+}
