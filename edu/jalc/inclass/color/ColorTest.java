d92599c734c1b34d64d47d7bec060c66eb390a99
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-10 08:06:52 -0600

    Finished ColorTest

--- edu/jalc/inclass/color/ColorTest.java
+++ edu/jalc/inclass/color/ColorTest.java
@@ -4,6 +4,26 @@ public class ColorTest{
 
   public void testGetRed(){
     System.out.println("Testing Color: getRed");
-    
+    Color color = new Color((byte)1, (byte)1, (byte)1);
+    assert(Color.getRed() == (byte)1);
+  }
+
+  public void testGetBlue(){
+    System.out.println("Testing Color: getBlue");
+    Color color = new Color((byte)1, (byte)1, (byte)1);
+    assert(Color.getBlue() == (byte)1);
+  }
+
+  public void testGetGreen(){
+    System.out.println("Testing Color: getGreen");
+    Color color = new Color((byte)1, (byte)1, (byte)1);
+    assert(Color.getGreen() == (byte)1);
+  }
+  public static void main(String[] args){
+    ColorTest colorTest = new ColorTest();
+    colorTest.testGetRed();
+    colorTest.testGetBlue();
+    colorTest.testGetGreen();
+    System.out.println("Test is complete");
   }
 }
