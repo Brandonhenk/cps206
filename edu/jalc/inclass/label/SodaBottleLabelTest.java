c36f7ecf8f20560e5419d23f39a6557e6c954211
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-09 22:31:53 -0600

    SodaBottleLabelTest

---
+++ edu/jalc/inclass/label/SodaBottleLabelTest.java
@@ -0,0 +1,12 @@
+package edu.jalc.inlcass.label;
+
+public class SodaBottleLabelTest{
+
+  public void testGetHeight(){
+    System.out.println("Testing Image: getHeight()");
+    Image image = new Image(1.0);
+    assert(Image.getHeight() == 1.0);
+  }
+
+  
+}
